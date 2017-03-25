446001e6ea03983f76d538e6f56d682de2d54f4f
Andrew Romanov <me@andrew-r.ru>
2017-03-25 13:57:57 +0300

    [#2] Add tests for pretty-json-stringify()

---
+++ cms/helpers/pretty-json-stringify.spec.js
@@ -0,0 +1,25 @@
+import prettyJSONStringify from './pretty-json-stringify';
+
+describe('prettyJSONStringify', () => {
+	test('should stringify data to JSON and format it with two space indentation', () => {
+		const data = {
+			firstName: 'Andrew',
+			lastName: 'Romanov',
+			age: 19,
+			interests: ['programming', 'music', 'design'],
+		};
+
+		const expectedResult = `{
+  "firstName": "Andrew",
+  "lastName": "Romanov",
+  "age": 19,
+  "interests": [
+    "programming",
+    "music",
+    "design"
+  ]
+}`;
+
+		expect(prettyJSONStringify(data)).toEqual(expectedResult);
+	});
+});
